1b40bd31a8dd0fcf29a5a073a9f3ca8537ee7837
Rian Rahman Al Aziz <rianrahman8243@gmail.com>
2026-04-09 10:08:09 +0700

    Implement user management functionality: CRUD operations for users

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> | Restoranku</title>
+    <title>@yield('title') | Restoranku</title>
 
     <link rel="icon" href="https://apps.codepolitan.com/sites/learn/uploads/original/2/logo-codepolitan-originals.png"
         type="image/x-icon">
